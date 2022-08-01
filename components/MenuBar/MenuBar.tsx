873506227d6b2c7745180d04281d902a1dcd1c9a
Richy Tomy <Richy_Tomy@mckinsey.com>
2022-07-31 21:30:45 -0700

    menu bar active

--- components/MenuBar/MenuBar.tsx
+++ components/MenuBar/MenuBar.tsx
@@ -1,13 +1,24 @@
+import clsx from 'clsx';
 import Link from 'next/link'
+import { useRouter } from "next/router";
 import styles from '../MenuBar/styles/MenuBar.module.css'
 
 const MenuBar = () => {
+    const router = useRouter();
     return (
       <div className={styles.menuBar}>
-          <Link href="/"><a className={styles.menuItem} >Home</a></Link>
-        <Link href="/about"><a className={styles.menuItem} >About</a></Link>
-        <Link href="/blog"><a className={styles.menuItem} >Blog</a></Link>
-        <Link href="/contact"><a className={styles.menuItem} >Contact</a></Link>
+          <Link href="/"><a className={`${router.pathname  === "/" 
+       ? `${styles.active}` 
+       : `${styles.nonActive}`}`}>Home</a></Link>
+        <Link href="/about"><a className={`${router.pathname  === "/about" 
+       ? `${styles.active}` 
+       : `${styles.nonActive}`}`} >About</a></Link>
+        <Link href="/blog"><a className={`${router.pathname  === "/blog" 
+       ? `${styles.active}` 
+       : `${styles.nonActive}`}`} >Blog</a></Link>
+        <Link href="/contact"><a className={`${router.pathname  === "/contact" 
+       ? `${styles.active}` 
+       : `${styles.nonActive}`}`} >Contact</a></Link>
       </div>
     );
   }
