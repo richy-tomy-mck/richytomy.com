954c01d631844add673869d75a4f4c6954d6c7e2
Richy Tomy <Richy_Tomy@mckinsey.com>
2022-07-31 20:55:19 -0700

    reset commits

--- components/MenuBar/MenuBar.tsx
+++ components/MenuBar/MenuBar.tsx
@@ -1,13 +1,13 @@
-import { clsx } from 'clsx';
+import Link from 'next/link'
 import styles from '../MenuBar/styles/MenuBar.module.css'
 
 const MenuBar = () => {
     return (
       <div className={styles.menuBar}>
-          <h1 className={styles.menuItem}><a href="/">Home</a></h1>
-        <h1 className={styles.menuItem} ><a href="/about">About</a></h1>
-        <h1 className={styles.menuItem} ><a href="/blog">Blog</a></h1>
-        <h1 className={styles.menuItem} ><a href="/contact">Contact</a></h1>
+          <Link href="/"><a className={styles.menuItem} >Home</a></Link>
+        <Link href="/about"><a className={styles.menuItem} >About</a></Link>
+        <Link href="/blog"><a className={styles.menuItem} >Blog</a></Link>
+        <Link href="/contact"><a className={styles.menuItem} >Contact</a></Link>
       </div>
     );
   }
